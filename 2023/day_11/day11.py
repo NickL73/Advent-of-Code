a1a54cb07cc378e7fb4b9157248b1b6aa26a049f
Nicholas Lunsford <nick.lunsford73@gmail.com>
2023-12-16 16:05:51 -0500

    Completes 23 Day 11 P2

--- 2023/day_11/day11.py
+++ 2023/day_11/day11.py
@@ -1,26 +1,32 @@
 #!/bin/bash/python3
 import sys
 
-
-def prep_grid(filename):
+def prepare_input(filename, multiplier):
     with open(filename, "r") as file:
         lines = file.readlines()
 
-    expanded = []
+    grid = []
     for line in lines:
-        line = list(line.strip())
-        if all(x == "." for x in line):
-            expanded.append(line)
-        expanded.append(line)
+        grid.append((line.strip()))
     
-    rotated = [[x[i] for x in expanded] for i in range(len(expanded[0]))]
-    expanded = []
-    for line in rotated:
-        if all(x == "." for x in line):
-            expanded.append(line)
-        expanded.append(line)
+    row_mult = [1 for x in range(len(grid))]
+    col_mult = [1 for x in range(len(grid[0]))]
+
+    # Check for empty rows in the grid
+    for i in range(len(grid)):
+        if all(x == "." for x in grid[i]):
+            row_mult[i] = multiplier
     
-    return expanded
+    # Check for empty columns
+    for i in range(len(grid[0])):
+        col = ""
+        for j in range(len(grid)):
+            col += grid[j][i]
+        
+        if all(x == "." for x in col):
+            col_mult[i] = multiplier
+    
+    return grid, row_mult, col_mult  
 
 def find_points(grid):
     coords = []
@@ -35,21 +41,27 @@ def find_points(grid):
     return coords
 
 
-def day11(filename):
-    grid = prep_grid(filename)
+def day11(filename, multiplier):
+    grid, row_mult, col_mult = prepare_input(filename, multiplier)
     points = find_points(grid)
 
     total = 0
     for i in range(len(points)):
         for j in range(i, len(points)):
-            disty = abs(points[i][0] - points[j][0])
-            distx = abs(points[i][1] - points[j][1])
+            min_row, max_row = sorted([points[i][0], points[j][0]])
+            min_col, max_col = sorted([points[i][1], points[j][1]])
 
-            total += (disty + distx)
-    
+            dist_col = sum(col_mult[min_col:max_col])
+            dist_row = sum(row_mult[min_row:max_row])
+
+            total += dist_col + dist_row
+
     return total
         
 
 if __name__=="__main__":
-    p1 = day11(sys.argv[1])
+    p1 = day11(sys.argv[1], 2)
     print(f"The answer for Part 1 is {p1}.")
+
+    p1 = day11(sys.argv[1], 1000000)
+    print(f"The answer for Part 2 is {p1}.")
