73fc8f2c40f8a2628978268fda4fc1cbd72d2c7c
Nicholas Lunsford <nick.lunsford73@gmail.com>
2024-12-02 18:09:24 -0500

    Completed 2024 D2P2

--- 2024/day_2/day2.py
+++ 2024/day_2/day2.py
@@ -2,6 +2,13 @@
 import sys
 import re
 
+def is_safe(line, ascending):
+    if ascending:
+        return all((earlier < later and later - earlier <= 3) for earlier, later in zip(line, line[1:]))
+    else:
+        return all((earlier > later and earlier - later <= 3) for earlier, later in zip(line, line[1:]))
+
+
 def day2(input_file):
     safe = 0
     safe_with_dampener = 0
@@ -10,13 +17,17 @@ def day2(input_file):
         lines = [[int(val) for val in re.split(r'\s+', line.strip())] for line in inp.readlines()]
     
     for line in lines:
-        if all(((earlier > later) and (earlier - later <= 3))for earlier, later in zip(line, line[1:])):
+        if is_safe(line, True) or is_safe(line, False):
             safe += 1
+            continue
 
-        if all(((earlier < later) and (later - earlier <= 3)) for earlier, later in zip(line, line[1:])):
-            safe += 1
+        for i in range(len(line)):
+            drop = line[:i] + line[i+1:]
+            if is_safe(drop, True) or is_safe(drop, False):
+                safe_with_dampener += 1
+                break
 
-    print(f"Part 1: {safe} - Part 2 {safe_with_dampener}")
+    print(f"Part 1: {safe} --- Part 2: {safe + safe_with_dampener}")
 
 if __name__ == "__main__":
     day2(sys.argv[1])
--- README.md
+++ README.md
@@ -12,13 +12,13 @@ The linked solutions below are the ones that initially passed each puzzle, and m
 
 ## 2024
 
-**Stars:** 2/50
+**Stars:** 4/50
 
 <center>
 
 | | | | | |
 |:-:|:-:|:-:|:-:|:-:|
-|Day 1 :star: <br /> [Python](2024/day_1/day1.py)|Day 2|Day 3|Day 4|Day 5|
+|Day 1 :star: <br /> [Python](2024/day_1/day1.py)|Day 2 :star: <br /> [Python](2024/day_2/day2.py)|Day 3|Day 4|Day 5|
 |Day 6|Day 7|Day 8|Day 9|Day 10|
 |Day 11|Day 12|Day 13|Day 14|Day 15|
 |Day 16|Day 17|Day 18|Day 19|Day 20|
